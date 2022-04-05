fd220462a638a78d785a9667968d27aee72b8b09
Tony Albacete <tbalbacete@gmail.com>
2022-04-05 10:03:31 -0400

    fixed nullable ArtistId

--- Muza.Data/Entities/ArtistRatingEntity.cs
+++ Muza.Data/Entities/ArtistRatingEntity.cs
@@ -13,13 +13,14 @@ namespace Muza.Data.Entities
         [Key]
         public int Id {get; set;}
 
+        [Required]
         [ForeignKey(nameof(Artist))]
-
-        public int? ArtistId {get; set;}
+        public int ArtistId {get; set;}
         public ArtistEntity Artist {get; set;}
 
         [Required]
         public int Rating {get; set;}
+        
         [Required]
         public DateTimeOffset CreatedUtc { get; set; }
 
