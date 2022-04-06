acbd6469389e80635d7e186aa1d374feaa201307
Eilers Fam <alexeilers@Alexs-iMac.local>
2022-04-06 10:10:24 -0400

    added year created property to artist

--- Muza.Models/Artist/ArtistListItem.cs
+++ Muza.Models/Artist/ArtistListItem.cs
@@ -10,6 +10,7 @@ namespace Muza.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Genre { get; set; }
+        public string YearCreated { get; set; }
         public string Description { get; set; }
     }
 }
